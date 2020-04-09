0f7ec193fc0f15e2f1dfd4771f4d74be803902af
Adhiraj Singh <adhirajsingh1206@gmail.com>
2020-04-09 22:49:27 +0530

    Added WebApp Basic Structure

---
+++ Webapp/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return render_template('main.html')
+
+    if request.method == 'POST':
+        link = request.form['link']
+        result = 88 
+        print(link , result) 
+        return render_template('main.html',original_input={'URl of post':link}, result=result)
+
+@app.route('/automated_testing', methods=['GET', 'POST'])
+def automated_testing():
+    if request.method == 'POST':
+        file = request.files['file'] 
+        print(content)
+        return jsonify(content)
+    if request.method == 'GET':
+        return "hello"
+
+if __name__ == '__main__':
+    app.run(debug = True)
