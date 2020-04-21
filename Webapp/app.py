77e3f46773d2dfffcca17292dcaf806bb576e279
Adhiraj Singh <adhirajsingh1206@gmail.com>
2020-04-21 16:10:55 +0530

    Changes for testing on heroku

--- Webapp/app.py
+++ Webapp/app.py
@@ -1,39 +1,7 @@
-import pickle
-import praw
-from praw.models import MoreComments
-
-from bs4 import BeautifulSoup
-import re
-import nltk
-from nltk.corpus import stopwords
-import gensim
 
 from flask import Flask, render_template, request, jsonify
 
-reddit = praw.Reddit(client_id="ffKcEa2xKfnhyg",
-                     client_secret="IJqQkTrDio0xKsKYKYmgeWSoOLM",
-                     user_agent="flair_predication",
-                     username="ASingh1206",
-                     password="g5gh#4$iQFGNBad")
-
-replace_by_space = re.compile('[/(){}\[\]\|@,;]')
-replace_symbol = re.compile('[^0-9a-z #+_]')
-STOPWORDS = set(stopwords.words('english'))
-
-def string_form(value):
-    return str(value)
-
-def clean_text(text):
-    #text = BeautifulSoup(text, "lxml").text # HTML decoding
-    text = text.lower() # lowercase text
-    text = replace_by_space.sub(' ', text) # replace certain symbols by space in text
-    text = replace_symbol.sub('', text) # delete symbols from text
-    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove STOPWORDS from text
-    return text
-
 app = Flask(__name__, template_folder='templates')
-
-model = pickle.load(open('Lr.pkl','rb'))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -43,31 +11,7 @@ def main():
 
     if request.method == 'POST':
         link = request.form['link']
-        submission = reddit.submission(url = link)
-        data = {}
-        data["title"] = str(submission.title)
-        data["url"] = str(submission.url)
-        data["body"] = str(submission.selftext)
-
-        submission.comments.replace_more(limit=None)
-        comment = ''
-        count = 0
-        for top_level_comment in submission.comments:
-            comment = comment + ' ' + top_level_comment.body
-            count+=1
-            if(count > 10):
-                break
-            
-        data["comment"] = str(comment)
-
-        data['title'] = clean_text(str(data['title']))
-        data['body'] = clean_text(str(data['body']))
-        data['comment'] = clean_text(str(data['comment']))
-        
-        combined_features = data["title"] + data["comment"] + data["body"] + data["url"]
-
-        result =  model.predict([combined_features])
-
+        result = 88
         print(link , result) 
         return render_template('main.html',original_input={'URl of post':link}, result=result)
 
--- Webapp/appBase.py
+++ Webapp/appBase.py
@@ -5,13 +5,13 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
-        return render_template('main2.html')
+        return render_template('main.html')
 
     if request.method == 'POST':
         link = request.form['link']
         result = 88 
         print(link , result) 
-        return render_template('main2.html',original_input={'URl of post':link}, result=result)
+        return render_template('main.html',original_input={'URl of post':link}, result=result)
 
 @app.route('/automated_testing', methods=['GET', 'POST'])
 def automated_testing():    
