cf95bea420124a7514976402364deee2f3faffc6
Adhiraj Singh <adhirajsingh1206@gmail.com>
2020-04-26 14:04:46 +0530

    Change in file structure and minor code changes

--- Webapp/app.py
+++ Webapp/app.py
@@ -1,46 +1,45 @@
+# Libraries below are for Reddit data scraping:
 import pickle
 import praw
 from praw.models import MoreComments
 
+# Libraries below are for Data Cleaning and formatting:
 from bs4 import BeautifulSoup
 import re
-import nltk
-from nltk.corpus import stopwords
 import gensim
-nltk.download('stopwords')
+from gensim import utils
+import gensim.parsing.preprocessing as gsp
 
+# Libraries below are essential for the WebApp: 
 from flask import Flask, render_template, request
 import json
 
-app = Flask(__name__, template_folder='templates')
-
+# Loading the Model file 
 model = pickle.load(open('RM.pkl','rb'))
 
+# Establishing connectiong with PRAW API
 reddit = praw.Reddit(client_id="ffKcEa2xKfnhyg", client_secret="IJqQkTrDio0xKsKYKYmgeWSoOLM",
                      user_agent="flair_predication", username="ASingh1206",
                      password="g5gh#4$iQFGNBad")
 
-
-replace_by_space = re.compile('[/(){}\[\]\|@,;]')
-replace_symbol = re.compile('[^0-9a-z #+_]')
-STOPWORDS = set(stopwords.words('english'))
+# Utiliy Functions to Fetch, Clean and Normalize Data:
+filters = [ gsp.strip_tags, gsp.strip_punctuation, gsp.strip_multiple_whitespaces,
+           gsp.strip_numeric, gsp.remove_stopwords, gsp.strip_short, gsp.stem_text ]
 
 def clean_text(text):
-    text = text.lower() # lowercase text
-    text = replace_by_space.sub(' ', text) # replace certain symbols by space in text
-    text = replace_symbol.sub('', text) # delete symbols from text
-    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove STOPWORDS from text
+    text = text.lower()
+    text = utils.to_unicode(text)
+    for filter in filters:
+        text = filter(text)
     return text
 
-
 def get_data(link):
-
     submission = reddit.submission(url = link)
     data = {}
     data["title"] = str(submission.title)
     data["url"] = str(submission.url)
     data["body"] = str(submission.selftext)
-
+      
     submission.comments.replace_more(limit=None)
     comment = ''
     count = 0
@@ -51,49 +50,50 @@ def get_data(link):
             break
         
     data["comment"] = str(comment)
-
     data['title'] = clean_text(str(data['title']))
     data['body'] = clean_text(str(data['body']))
-    data['comment'] = clean_text(str(data['comment']))
-    
+    data['comment'] = clean_text(str(data['comment']))    
     combined_features = data["title"] + data["comment"] + data["body"] 
-
     return combined_features
+
+# Initialzing the App
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
-        return render_template('main.html')
+        return render_template('main.html') # Renders the Front End to receive the link.
 
     if request.method == 'POST':
         link = request.form['link']
-        cb_features = get_data(link)
-        result =  model.predict([cb_features])
+        cb_features = get_data(link)    # Function returns the relevent data scrapped and cleaned from Reddit
+        result =  model.predict([cb_features])    # Calls the Random Forest Model and returns preddicted Flair
         print(link , result) 
         return render_template('main.html',original_input={'URl of post':link}, result=result)
 
-@app.route('/automated_testing', methods=['GET', 'POST'])
+
+
+@app.route('/automated_testing', methods=['GET', 'POST']) # Endpoint for Automated Testing
 def automated_testing():    
     if request.method == 'POST':
-        file = request.files['upload_file'] 
-        #content = str(file.read())
-        links = file.readlines()
+        file = request.files['upload_file']     # Loades the uploaded file
+        links = file.readlines()    # Breaks the text into multiple lines and removes \n
         #print(links)
         json_dict = {}
-        for url in links:
+        for url in links:   # Traversing through the multiple lines in the file
             url = str(url)
-            url = url[2:-3]
-            print(url)
-            cb_features = get_data(str(url))
-            result =  str(model.predict([cb_features]))
+            url = url[2:-3] 
+            #print(url)
+            cb_features = get_data(str(url))    # Function returns the relevent data scrapped and cleaned from Reddit
+            result =  str(model.predict([cb_features]))     # Calls the Model and returns preddicted Flair 
             result = result[2:-2]
-            print(result)
-            json_dict[url] = result
+            #print(result)
+            json_dict[url] = result     # Appends the result to a dictionary
         json_dict = json.dumps(json_dict)
-        return json.loads(json_dict)
+        return json.loads(json_dict)    # Returns a json file with all the links and their corresponding Flairs
     
     if request.method == 'GET':
         return "Please send a post request with a text file containing links to r/india."
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = False)
--- Webapp/test_automatedTesting.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/automated_testing"
-
-
-files = {'upload_file': open('file.txt','rb')}
-r = requests.post(url, files=files)
-print(r.text)
