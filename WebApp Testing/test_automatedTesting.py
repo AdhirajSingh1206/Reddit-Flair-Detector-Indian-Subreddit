bfecf7f5e25c96a61375a03b9cb40573ae42b850
Adhiraj Singh <adhirajsingh1206@gmail.com>
2020-04-26 20:57:11 +0530

    Minor Changes

--- WebApp Testing/test_automatedTesting.py
+++ WebApp Testing/test_automatedTesting.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:5000/automated_testing"
+url = "https://flair--predictor.herokuapp.com/automated_testing"
 
 
 files = {'upload_file': open('file.txt','rb')}
